869cadf6e1b160dda0406e2c4cb71fbff14a2365
csalzsieder <csalzsieder@inspirato.com>
2019-10-28 09:13:12 -0600

    Added postman grammar

---
+++ postman.py
@@ -0,0 +1,22 @@
+#imports the library
+from dragonfly import (Grammar, AppContext, MappingRule, Integer, Key, Text, Dictation, Choice, Pause)
+
+class CodeMappings(MappingRule):
+    mapping = {  
+            'run it': Key('c-enter'),
+        }
+    extras=[
+        Integer('tab', 1, 10),
+        Integer('number', 1, 9999),
+        Dictation("text")
+    ]
+
+context = AppContext(executable='Postman')
+grammar=Grammar('Post man',context=context)
+grammar.add_rule(CodeMappings())
+grammar.load()
+
+def unload():
+    global grammar
+    if grammar: grammar.unload()
+    grammar = None
