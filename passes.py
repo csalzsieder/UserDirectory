7b6ce7fa077702d1d491fc70e91d83c1c9a2ea8f
Craig Salzsieder <csalzsieder@inspirato.com>
2021-09-04 13:42:17 -0600

    wip

--- passes.py
+++
@@ -1,6 +0,0 @@
-prod_pass = "Conec#20!!io20"
-sand_pass = "Conec#20!!io20"
-craig_pass = "conec#20"
-in_pass = "conec#25"
-# testy_pass = "Inspirato01"
-# test - dapi4b8cb315014487c54170ae9563451f79
